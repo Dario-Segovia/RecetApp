c5a7ce959d75e1e5b47f514aa2407246f52a5f21
Dario-Segovia <dariosegovia194@gmail.com>
2024-11-27 18:51:20 +0100

    actualizado

--- app/src/main/java/com/santosgo/recetapp/MainActivity.kt
+++ app/src/main/java/com/santosgo/recetapp/MainActivity.kt
@@ -6,7 +6,10 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/santosgo/recetapp/SplashActivity.kt
+++ app/src/main/java/com/santosgo/recetapp/SplashActivity.kt
@@ -9,12 +9,14 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_splash) // Aquí pones el layout de tu splash
 
+        // Usamos un Handler para retrasar la transición a MainActivity
         Handler(Looper.getMainLooper()).postDelayed({
+            // Iniciamos la MainActivity después de 2 segundos
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            finish()
-        }, 3000) // Tiempo de espera de 3 segundos
+            finish() // Terminamos la SplashActivity para que no se quede en el stack
+        }, 2000) // 2000ms = 2 segundos
     }
 }
